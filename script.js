e0044577c9a9f40fe2eecb0dbf26795ded49885c
Lisa-Doroshenko <127964496+Lisa-Doroshenko@users.noreply.github.com>
2023-03-27 19:13:03 +0700

    удалено дублирование
    удалено дублирование

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const someValue = 7n;
 {
 console.log(typeof someValue== "BigInt" || 
 typeof someValue== "number");
-  console.log(typeof someValue== "BigInt" || 
-typeof someValue== "number");
+number");
 }
 
